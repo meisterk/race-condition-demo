df2398a6422b4e3150eac36bb40c05e38cd03d1d
meisterk <franz@kohnlehome.de>
2019-11-06 11:56:29 +0100

    Add versions

--- src/Task.java
+++ src/Task.java
@@ -1,21 +1,45 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Task implements Runnable {
     private int count;
+    private Lock lock;
 
     public Task() {
         count = 0;
+        lock = new ReentrantLock();
     }
 
     @Override
     public void run() {
-        //increment();
+        version3();
+        System.out.println(count);
+    }
+
+    private void version0() {
+        count++;
+    }
+
+    private void version1() {
+        increment();
+    }
+
+    private synchronized void increment() {
+        count++; // Critical Code
+    }
+
+    private void version2() {
         synchronized (this) {
-            count++; // Critical Code
-            System.out.println(count);
+            count++;
         }
     }
 
-//    private synchronized void increment() {
-//        count++; // Critical Code
-//        System.out.println(count);
-//    }
+    private void version3() {
+        // Code sperren
+        lock.lock();
+        count++;
+        // Code freigeben
+        lock.unlock();
+    }
+
 }
